4856125e284a47f48e59310caf9259d610a29a00
Andrei Elias <dreeielias@gmail.com>
2024-03-10 12:36:04 -0300

    usanso let

---
+++ aula6/testes.js
@@ -0,0 +1,4 @@
+console.log('João nasceu em 1984.');
+console.log('Em 2000 João conheceu Maria.');
+console.log('João casou-se com Maria em 2012.');
+console.log('O folho de João se chama Eduardo.');
