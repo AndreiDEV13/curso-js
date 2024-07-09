722dfedd6f063ff54dbc34db39bf120e740d903c
Andrei Elias <dreeielias@gmail.com>
2024-07-08 21:44:51 -0300

    tipos primitivos

---
+++ aula9/index.js
@@ -0,0 +1,17 @@
+const nome = 'Andrei'; // string
+const nome2 = "Andrei"; // string
+const nome1 = `Andrei`; // string
+
+const num1 = 10; // number
+const num2 = 10.53; // number
+
+let nomeAluno; // undefined
+let sobrenomeAluno = null; // nulo -> não aponta para lugar nenhum na memoria
+const aprovado = false; // true or false (lógico)
+
+let a = 2;
+const b = a;
+console.log(a, b); // 2, 2
+
+a = 3;
+console.log(a, b); // 3, 2
